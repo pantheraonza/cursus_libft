0fa9172d0a47ad9c90c5fb55198048b1ad8da8ad
pantheraonza <cristinaqr@gmail.com>
2026-06-04 12:15:12 +0200

    Added new version ft_isalpha

---
+++ backup_part1/ft_isdigit.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: criredon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 16:11:47 by criredon          #+#    #+#             */
+/*   Updated: 2026/06/03 16:11:49 by criredon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+
+
+
+
+/* ******************************* M A N U A L *********************************
+NAME
+
+
+SYNOPSIS (LIBRARY & FUNCTION PROTOTYPE)
+
+
+DESCRIPTION
+
+
+RETURN VALUES
+
+
+42SUBJECT BY CRIREDON
+For character classification functions (isalpha, isdigit, isalnum,
+isascii, isprint), the return value must be:
+1 if the character meets the tested condition.
+0 if the character does not meet it. 
+
+ERRORS
+Not mentioned by the "man".
+
+NOTES
+
+
+EXTRA NOTES BY CRIREDON
+
+
+
+******************************** M A N U A L ******************************** */
